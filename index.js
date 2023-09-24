2c43c8e8251a5436f24dc8df29e0514803452e86
Ivan Babkov <babkov1738@gmail.com>
2023-09-24 20:35:01 +0300

    did 3 steps

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
+import _ from 'lodash'
 export default function solution(content) {
   // BEGIN
-  console.log(content)
-  // END
+// step 1
+const rows = content.split('\n').slice(2)
+const data = rows.map((row) => row.split('|').slice(1).slice(0, -1).map((row) => row.trim()))
+console.log(`${data.length}`);
+// step 2 
+const rasteniya = _.uniq(data.map((row) => 
+row[0].charAt().toUpperCase() + row[0].slice(1).toLowerCase()).sort());
+console.log(`${rasteniya.join(', ')}`);
+//step 3
+const safePlants = data.map((row) => row[4] === 'Нет')
+const dangerousRasteniya = data.length - safePlants.length;
+console.log(`Безопасные растения: ${(safePlants.length / data.length) * 100}%`);
+console.log(`Опасные растения: ${(dangerousRasteniya / data.length) * 100}%`);
+// step 4
+// END
 }
